d7ef725693912c56518f2a18d6bde15472175ca7
b3717898 <b3717898@163.com>
2016-10-19 14:48:01 +0800

    commit junit

--- test/com/yum/boh/update/TestUpdate.java
+++ test/com/yum/boh/update/TestUpdate.java
@@ -2,8 +2,6 @@ package com.yum.boh.update;/**
  * Created by Angel on 6/20/15.
  */
 
-import com.yum.boh.fund.service.update.TemporaryBOHUpdateService;
-import com.yum.boh.fundgen.model.ds.FundTDownloadTask;
 import org.junit.Assert;
 import org.junit.Test;
 
