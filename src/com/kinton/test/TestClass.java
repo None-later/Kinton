b2b2fb7afd874c2132490d666d6869f438924b59
b3717898 <b3717898@163.com>
2016-10-19 16:17:55 +0800

    commit files 4 test framework

--- src/com/kinton/test/TestClass.java
+++ src/com/kinton/test/TestClass.java
@@ -3,5 +3,13 @@ package com.kinton.test;
 /**
  * Created by Angel on 9/21/16.
  */
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestClass {
+    @Test
+    public void test() {
+        System.out.println("【Azure POC】testClass is executing.");
+        Assert.assertTrue(true);
+    }
 }
